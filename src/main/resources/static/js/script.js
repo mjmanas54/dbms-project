7aa3d63586e02979c5c5d9c94ca74fac0aca4666
Manas Jayaswal <mjmanas54@gmail.com>
2023-10-23 18:46:52 +0530

    add search functionality in navbar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,5 @@
 const navbar = document.querySelector('#navBar');
 const content = document.querySelector('#content-id');
-console.log("hello");
 const t = navbar.offsetTop;
 
 function stickynavbar() {
@@ -13,3 +12,54 @@ function stickynavbar() {
     }
 }
 window.addEventListener('scroll', stickynavbar);
+
+const search=()=>{
+    const searchElement = document.getElementById("search-input");
+    const restaurants = document.getElementById("restaurants");
+    const items = document.getElementById("items");
+    const searchResults = document.getElementById("search-result");
+
+    let query = searchElement.value;
+    let val1 = false;
+    let val2 = false;
+    if(query=="") {
+        searchResults.classList.remove("show");
+    }else{
+        let url = `http://localhost:8090/search/${query}`;
+
+        fetch(url)
+            .then((response)=>{
+                return response.json();
+            })
+            .then((data)=>{
+                let text = `<div class="list-group">`;
+                if(data.length===0){
+                    val1 = true;
+                }
+                data.forEach((restaurant)=>{
+                    text += `<a href="#" class="list-group-item list-group-action">${restaurant.restaurantName}`;
+                })
+                text += `</div>`;
+                restaurants.innerHTML = text;
+
+            })
+        let itemurl = `http://localhost:8090/searchItem/${query}`;
+        fetch(itemurl)
+            .then((response)=>{
+                return response.json();
+            })
+            .then((data)=>{
+                let text = `<div class="list-group">`;
+                if(data.length===0){
+                    val2 = true;
+                }
+                data.forEach((items)=>{
+                    text += `<a href="#" class="list-group-item list-group-action">${items.itemName}`;
+                })
+                text += `</div>`;
+                items.innerHTML = text;
+
+            })
+        searchResults.classList.add("show");
+    }
+}
